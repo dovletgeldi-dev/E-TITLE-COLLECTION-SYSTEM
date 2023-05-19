f0491ae1df6e53916f6ed111c52903ac5eee7492
Lum Wai Keen <89341748+LumWaiKeen@users.noreply.github.com>
2023-05-19 22:03:50 +0800

    Add files via upload

---
+++ E-TITLE-COLLECTION-SYSTEM-main/scripts/main.js
@@ -0,0 +1,8 @@
+"use strict";
+
+const toggleBtn = document.getElementsByClassName("toggle-button")[0];
+const navbarLinks = document.getElementsByClassName("navbar-links")[0];
+
+toggleBtn.addEventListener("click", () => {
+  navbarLinks.classList.toggle("active");
+});
