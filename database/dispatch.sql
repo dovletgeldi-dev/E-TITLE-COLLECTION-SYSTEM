776db75813beaf12abcadf8ac98786c6ea1f5a1c
Dovlet <dovlethelegend@gmail.com>
2023-06-04 14:09:00 +0800

    updated dispatch table

--- database/dispatch.sql
+++ database/dispatch.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: May 27, 2023 at 01:33 PM
+-- Generation Time: Jun 04, 2023 at 08:08 AM
 -- Server version: 10.4.28-MariaDB
 -- PHP Version: 8.2.4
 
@@ -28,19 +28,22 @@ SET time_zone = "+00:00";
 --
 
 CREATE TABLE `dispatch` (
+  `id` int(11) NOT NULL,
   `dispatch_nric` varchar(12) NOT NULL,
   `dispatch_name` varchar(50) NOT NULL,
   `dispatch_phone_no` varchar(11) NOT NULL,
-  `organization_name` varchar(50) NOT NULL,
-  `dispatch_img` varchar(50) NOT NULL
+  `organization_name` varchar(50) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `dispatch`
 --
 
-INSERT INTO `dispatch` (`dispatch_nric`, `dispatch_name`, `dispatch_phone_no`, `organization_name`, `dispatch_img`) VALUES
-('132213213214', 'Haji', '01728209533', 'Saul Goodman', '');
+INSERT INTO `dispatch` (`id`, `dispatch_nric`, `dispatch_name`, `dispatch_phone_no`, `organization_name`) VALUES
+(1, '010916231213', 'Steph Curry', '60172321321', 'Golden State Warriors'),
+(2, '990416213312', 'Michael Schumacher', '60173122132', 'Ferrari'),
+(3, '851130123213', 'Lionel Messi', '60173212313', 'Barcelona FC'),
+(4, '610109231231', 'Alex Simple', '60132132133', 'Natus Vincere');
 
 --
 -- Indexes for dumped tables
@@ -50,7 +53,17 @@ INSERT INTO `dispatch` (`dispatch_nric`, `dispatch_name`, `dispatch_phone_no`, `
 -- Indexes for table `dispatch`
 --
 ALTER TABLE `dispatch`
-  ADD PRIMARY KEY (`dispatch_nric`);
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `dispatch`
+--
+ALTER TABLE `dispatch`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
